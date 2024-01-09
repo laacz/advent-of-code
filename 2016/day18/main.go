f091df8a423707a87ec921521b36750d2d4961b4
Kaspars Foigts <laacz@laacz.lv>
2024-01-09 15:08:43 +0200

    2016 - day17

--- 2016/day18/main.go
+++ 2016/day18/main.go
@@ -11,33 +11,18 @@ func parse(input string) (string, int) {
 	return util.GetLines(input)[0], util.Atoi(util.GetLines(input)[1])
 }
 
-func nextRow(prevrow string) (string, int) {
+func nextRow(pr string) (string, int) {
+	prevrow := []byte("." + pr + ".")
 	ret := ""
 
-	for r := 0; r < len(prevrow); r++ {
-		m := ""
-		if r < 1 {
-			m += "."
-		} else {
-			m += string(prevrow[r-1])
-		}
+	for r := 0; r < len(prevrow)-2; r++ {
+		w := uint32(prevrow[r])<<16 + uint32(prevrow[r+1])<<8 + uint32(prevrow[r+2])
 
-		m += string(prevrow[r])
-
-		if r+1 >= len(prevrow) {
-			m += "."
-		} else {
-			m += string(prevrow[r+1])
-		}
-
-		trapma := map[string]bool{
-			"^^.": true,
-			".^^": true,
-			"^..": true,
-			"..^": true,
-		}
-
-		if trapma[m] {
+		// ..^ 3026526
+		// .^^ 3038814
+		// ^.. 6172206
+		// ^^. 6184494
+		if w == 3026526 || w == 3038814 || w == 6172206 || w == 6184494 {
 			ret += "^"
 		} else {
 			ret += "."
